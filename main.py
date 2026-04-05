64a7ff5e4a6702d3761a3c3b69b4b3633e1ca1e3
bada-dev <badatyagi2011@gmail.com>
2026-04-05 11:19:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-ADMIN_PASSWORD = "whisper123"  # Change this
+ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "fallback123")  # Change this
 DATA_FILE = "updates.json"
 
 def load_updates():
